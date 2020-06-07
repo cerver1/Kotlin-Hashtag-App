4b81e65a0221b602858a8335a7ceebc1d32f495c
Cerve <59832296+cerver1@users.noreply.github.com>
2020-06-06 21:32:11 -0400

    Update MainActivity.kt
    TO_DO_ added 9:31pm 6/6/20

--- app/src/main/java/com/fair/hastag_randomizer/MainActivity.kt
+++ app/src/main/java/com/fair/hastag_randomizer/MainActivity.kt
@@ -10,6 +10,18 @@ import androidx.appcompat.app.AppCompatActivity
 import com.fair.hastag_randomizer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    
+     /**
+     
+         TO_DO_
+
+         Needs README update 
+
+         Needs Radio buttons for user output size selection
+
+         Needs to be modeled after notes app OnePlus
+     
+     */
 
     private lateinit var myClipboard: ClipboardManager
     private lateinit var binding: ActivityMainBinding
