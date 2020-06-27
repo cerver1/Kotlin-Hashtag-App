a80ea4b0953f6699489c8eb428f842d9d947efbb
Cerve <59832296+cerver1@users.noreply.github.com>
2020-06-27 11:44:48 -0400

    size dialog option added 6/8/20 still needs backend functionality with shared preference. consider room database for hashtag save instead of shared preference.

--- app/src/main/java/com/fair/hastag_randomizer/view/MainFragment.kt
+++ app/src/main/java/com/fair/hastag_randomizer/view/MainFragment.kt
@@ -2,20 +2,51 @@ package com.fair.hastag_randomizer.view
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.fair.hastag_randomizer.R
+import com.fair.hastag_randomizer.databinding.FragmentMainBinding
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 
 class MainFragment: Fragment(R.layout.fragment_main) {
+    
+    private var _binding: FragmentMainBinding? = null
+    private val viewBinding get() = _binding!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentMainBinding.bind(view)
+
+        viewBinding.apply {
+
+            randomizeBtn.setOnClickListener {
+                val myList = listOf("#cerve","#instagram","#kotlin")
+                myList.forEach{
+                    addChip(it , chipContainer)
+
+                }
+
+            }
+
+        }
 
 
 
     }
 
+    fun addChip(item: String, group: ChipGroup){
+        val chip = Chip(context)
+        chip.text = item
+        chip.chipIcon = ContextCompat.getDrawable(requireContext(), R.drawable.ic_launcher_foreground)
+
+        chip.isCheckable = false
+        group.addView(chip as View)
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
+        _binding = null
 
     }
 }
