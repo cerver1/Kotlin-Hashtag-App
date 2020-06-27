03003e557951fae307cbd24fa71b2fb680a9e98b
Cerve <59832296+cerver1@users.noreply.github.com>
2020-06-27 10:52:42 -0400

    size dialog option added 6/8/20 still needs backend functionality with shared preference. consider room database for hashtag save instead of shared preference.

--- app/src/main/java/com/fair/hastag_randomizer/view/MainActivity.kt
+++ app/src/main/java/com/fair/hastag_randomizer/view/MainActivity.kt
@@ -5,7 +5,6 @@ import android.content.ClipboardManager
 import android.os.Bundle
 import android.view.WindowManager
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.fair.hastag_randomizer.databinding.ActivityMainBinding
 import com.fair.hastag_randomizer.repository.Randomize
@@ -37,42 +36,45 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         saveNew = SharedQuery(this)
 
-        window.apply {
-            setFlags(
-                WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,
-                WindowManager.LayoutParams.TYPE_STATUS_BAR
-            )
-        }
-
+        //window.apply {
+        //  setFlags(
+        //     WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,
+        //     WindowManager.LayoutParams.TYPE_STATUS_BAR
+        //)
+        //}
+        /**
         myClipboard = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
 
         with(binding) {
-            buttonClear.setOnClickListener { textInput.setText("") }
-            buttonRandom.setOnClickListener {
+        buttonClear.setOnClickListener { textInput.setText("") }
+        buttonRandom.setOnClickListener {
 
-                val validTags = rand.validateHashTagList(textInput.text.toString())
-                val randomTags = rand.randomizedHashTagList(validTags,"random")
+        val validTags = rand.validateHashTagList(textInput.text.toString())
+        val randomTags = rand.randomizedHashTagList(validTags,"random")
 
-                textOutput.text = rand.finalizeHashTags(randomTags.second)
-                // textOutputSized.text = randomTags.first.toString()
+        textOutput.text = rand.finalizeHashTags(randomTags.second)
+        // textOutputSized.text = randomTags.first.toString()
 
-                copyTxt(textOutput)
-                toast("Randomized and Copied")
-            }
-            buttonSave.setOnClickListener {
-                    saveNew.searchHash(textInput.text.toString(), textOutputSized )
-
-            }
-            buttonSize.setOnClickListener {
-                SizeDialog(this@MainActivity).showSizeDialog()
-            }
+        copyTxt(textOutput)
+        toast("Randomized and Copied")
         }
-    }
+        buttonSave.setOnClickListener {
+        saveNew.searchHash(textInput.text.toString(), textOutputSized )
 
-    private fun copyTxt(text: TextView) {
+        }
+        buttonSize.setOnClickListener {
+        SizeDialog(this@MainActivity).showSizeDialog()
+        }
+        }
+        }
+
+        private fun copyTxt(text: TextView) {
         val myClip = ClipData.newPlainText("NewRandom", text.text)
         myClipboard.setPrimaryClip(myClip!!)
-    }
+        }
 
-    
+
+        }
+         */
+    }
 }
---
+++ app/src/main/java/com/fair/hastag_randomizer/view/MainFragment.kt
@@ -0,0 +1,21 @@
+package com.fair.hastag_randomizer.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.fair.hastag_randomizer.R
+
+class MainFragment: Fragment(R.layout.fragment_main) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+    }
+}
