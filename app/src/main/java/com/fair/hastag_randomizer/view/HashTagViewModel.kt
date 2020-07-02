4a00885bca759bdf6ccc074a687885d718aab14e
Cerve <59832296+cerver1@users.noreply.github.com>
2020-07-02 13:01:24 -0400

    set up for saving data - ViewModel dependencies added

---
+++ app/src/main/java/com/fair/hastag_randomizer/view/HashTagViewModel.kt
@@ -0,0 +1,9 @@
+package com.fair.hastag_randomizer.view
+
+import androidx.lifecycle.ViewModel
+import com.fair.hastag_randomizer.repository.RandomizeRepository
+
+class HashTagViewModel(private val repository: RandomizeRepository): ViewModel() {
+
+
+}
