54ec6b12dca6523fe17ada898f06d101cb3c1724
Cerve <59832296+cerver1@users.noreply.github.com>
2020-07-02 13:53:02 -0400

    set up for saving data - recyclerview setup to save display saved data

---
+++ app/src/main/java/com/fair/hastag_randomizer/repository/storage/RandomizeRecyclerAdapter.kt
@@ -0,0 +1,37 @@
+package com.fair.hastag_randomizer.repository.storage
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.fair.hastag_randomizer.R
+import com.fair.hastag_randomizer.view.HashTagViewModel
+import kotlinx.android.synthetic.main.recycler_storage.view.*
+import kotlinx.coroutines.processNextEventInCurrentThread
+import kotlin.random.Random
+
+class RandomizeRecyclerAdapter(var hashtag: List<RandomizeEntity>, private val viewModel: HashTagViewModel): RecyclerView.Adapter<RandomizeRecyclerAdapter.RandomizeViewHolder>() {
+
+
+
+
+
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RandomizeViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_storage, parent, false)
+        return RandomizeViewHolder(view)
+    }
+
+    override fun getItemCount() = hashtag.size
+
+    override fun onBindViewHolder(holder: RandomizeViewHolder, position: Int) {
+        val currentHashtag = hashtag[position]
+        holder.itemView.hashContainer.text = currentHashtag.hashtag
+
+    }
+
+    inner class RandomizeViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+}
