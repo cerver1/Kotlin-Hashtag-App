86d5fa34c520439c8aaaf8cfad8cef03171819f2
Cerve <59832296+cerver1@users.noreply.github.com>
2020-08-28 00:12:38 -0400

    8/28/2020 : search-view implementation : crash free

--- app/src/main/java/com/fair/hastag_randomizer/view/StorageFragment.kt
+++ app/src/main/java/com/fair/hastag_randomizer/view/StorageFragment.kt
@@ -1,8 +1,13 @@
 package com.fair.hastag_randomizer.view
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.widget.SearchView
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -47,41 +52,6 @@ class StorageFragment: Fragment(R.layout.fragment_storage) {
                 Navigation.findNavController(view).navigate(R.id.action_storageFragment_to_mainFragment)
             }
 
-            storeToolbar.setOnMenuItemClickListener { item ->
-                when (item.itemId) {
-                    R.id.action_search -> {
-                        (item as SearchView).setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-                            override fun onQueryTextSubmit(p0: String?): Boolean {
-                                return true
-                            }
-
-                            override fun onQueryTextChange(newText: String?): Boolean {
-                                if (newText.isNullOrEmpty()) {
-                                    displayList.clear()
-                                    val search = newText?.toLowerCase(Locale.ROOT)
-                                    arrayList.forEach { obj ->
-
-                                            if (obj.hashtag.toLowerCase(Locale.ROOT).contains(search!!)) {
-                                                displayList.add(obj)
-                                            }
-                                    }
-
-                                    savedRecycler.adapter?.notifyDataSetChanged()
-
-                                }
-                                else {
-                                    displayList.clear()
-                                    displayList.addAll(arrayList)
-                                    savedRecycler.adapter?.notifyDataSetChanged()
-                                }
-                                return true
-                            }
-                        })
-                        true }
-                    else -> super.onOptionsItemSelected(item)
-                }
-            }
-
             val adapter = RandomizeRecyclerAdapter(arrayListOf(), viewModel)
 
             savedRecycler.layoutManager = LinearLayoutManager(context)
@@ -96,6 +66,22 @@ class StorageFragment: Fragment(R.layout.fragment_storage) {
 
         }
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        (menu.findItem(R.id.action_search).actionView as SearchView).apply {
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextChange(p0: String?): Boolean {
+                    return false
+                }
+
+                override fun onQueryTextSubmit(p0: String?): Boolean {
+                    return false
+                }
+            })
+        }
     }
 
     override fun onDestroyView() {
